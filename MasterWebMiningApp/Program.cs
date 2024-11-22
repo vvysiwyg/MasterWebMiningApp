b3ef4e07403146ed7c65d091c56b3abd2505549d
Artyom Koziev <artem891921@mail.ru>
2024-11-22 20:46:24 +0300

    Initial project configuration

--- MasterWebMiningApp/Program.cs
+++ MasterWebMiningApp/Program.cs
@@ -1,11 +1,18 @@
 using MasterWebMiningApp;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddLocalization();
 
-await builder.Build().RunAsync();
+WebAssemblyHost host = builder.Build();
+
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("ru-RU");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("ru-RU");
+
+await host.RunAsync();
